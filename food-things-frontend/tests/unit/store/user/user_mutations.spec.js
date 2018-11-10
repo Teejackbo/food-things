cb9fe13792f94790a50b75bc868e9ed1910d8b91
Jack Coldrick <jcldrk@gmail.com>
2018-11-10 10:27:39 +0000

    Write tests for user mutations.

---
+++ food-things-frontend/tests/unit/store/user/user_mutations.spec.js
@@ -0,0 +1,34 @@
+import mutations from '@/store/modules/user/mutations'
+import TYPES from '@/store/modules/user/types'
+
+describe('User Mutations', () => {
+  let state
+  const user = {
+    id: 1,
+    first_name: 'Jack',
+    last_name: 'Coldrick',
+    username: 'jack',
+    email: 'jack@foodthings.com',
+  }
+
+  beforeEach(() => {
+    state = {
+      user: null,
+    }
+  })
+
+  describe('User Stored', () => {
+    it('Should set the user in state.', () => {
+      mutations[TYPES.STORE_USER_MUTATION](state, user)
+      expect(state.user).toBe(user)
+    })
+  })
+
+  describe('User Deleted', () => {
+    it('Should remove the user from state.', () => {
+      state.user = user
+      mutations[TYPES.DELETE_USER_MUTATION](state)
+      expect(state.user).toEqual({})
+    })
+  })
+})
