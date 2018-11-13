6a845350db83e76410e6b4d8f400e8780b543d7b
Jack Coldrick <jcldrk@gmail.com>
2018-11-13 20:10:59 +0000

    Add snapshot tests for Alert.

--- food-things-frontend/tests/unit/components/Alert.spec.js
+++ food-things-frontend/tests/unit/components/Alert.spec.js
@@ -12,11 +12,59 @@ describe('Alert', () => {
           message: 'This is an alert.',
         },
       },
-      mocks: {
-        $store,
-      },
+      mocks: { $store },
     })
   }
+
+  it('Should match snapshot with no class.', () => {
+    const wrapper = shallowMount(Alert, {
+      propsData: {
+        alert: {
+          message: 'This is an alert',
+          timeout: 3000,
+        },
+      },
+      mocks: { $store },
+    })
+
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('Should match snapshot with a type of success.', () => {
+    const wrapper = getWrapper()
+
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('Should match snapshot with a type of warning.', () => {
+    const wrapper = shallowMount(Alert, {
+      propsData: {
+        alert: {
+          message: 'This is an alert',
+          timeout: 3000,
+          type: 'warning',
+        },
+      },
+      mocks: { $store },
+    })
+
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('Should match snapshot with a type of error.', () => {
+    const wrapper = shallowMount(Alert, {
+      propsData: {
+        alert: {
+          message: 'This is an alert',
+          timeout: 3000,
+          type: 'error',
+        },
+      },
+      mocks: { $store },
+    })
+
+    expect(wrapper.element).toMatchSnapshot()
+  })
 
   it('Renders a div.', () => {
     const wrapper = getWrapper()
