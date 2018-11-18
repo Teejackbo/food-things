c53ef753a0cbf4838ad436116dc306b07e81c715
Jack Coldrick <jcldrk@gmail.com>
2018-11-18 11:58:36 +0000

    Fix E2E tests.

--- food-things-frontend/tests/e2e/specs/sign_up.js
+++ food-things-frontend/tests/e2e/specs/sign_up.js
@@ -13,74 +13,81 @@ describe('Sign Up', () => {
     cy.get('button.success')
   })
 
-  it('Should display an error without a first name.', () => {
-    cy.route({
-      method: 'POST',
-      url: '**/users/register',
-      response: 'fx:sign_up/no_first_name.json',
-      status: 400,
-    })
-
-    cy.get('[name="last_name"]').type('last name')
+  it('Should display an error without a first name on submit.', () => {
+    cy.get('[name="last_name"]').type('last')
     cy.get('[name="email"]').type('email@email.email')
     cy.get('[name="username"]').type('username')
     cy.get('[name="password"]').type('password')
     cy.get('button.success').click()
 
-    cy.contains('Please enter a first name.').should('have.class', 'error')
+    cy.contains('Please enter your first name.')
+    cy.get('[name="first_name"]').should('have.class', 'error')
   })
 
-  it('Should display an error without a last name.', () => {
-    cy.route({
-      method: 'POST',
-      url: '**/users/register',
-      response: 'fx:sign_up/no_last_name.json',
-      status: 400,
-    })
+  it('Should display an error without a first name on blur.', () => {
+    cy.get('[name="first_name"]').click()
+    cy.get('[name="last_name"]').click()
 
-    cy.get('[name="first_name"]').type('first name')
+    cy.contains('Please enter your first name.')
+    cy.get('[name="first_name"]').should('have.class', 'error')
+  })
+
+  it('Should display an error without a last name on submit.', () => {
+    cy.get('[name="first_name"]').type('first')
     cy.get('[name="email"]').type('email@email.email')
     cy.get('[name="username"]').type('username')
     cy.get('[name="password"]').type('password')
     cy.get('button.success').click()
 
-    cy.contains('Please enter a last name.').should('have.class', 'error')
+    cy.contains('Please enter your last name.')
+    cy.get('[name="last_name"]').should('have.class', 'error')
   })
 
-  it('Should display an error without an email.', () => {
-    cy.route({
-      method: 'POST',
-      url: '**/users/register',
-      response: 'fx:sign_up/no_email.json',
-      status: 400,
-    })
+  it('Should display an error without a last name on blur.', () => {
+    cy.get('[name="last_name"]').click()
+    cy.get('[name="first_name"]').click()
 
-    cy.get('[name="first_name"]').type('first name')
-    cy.get('[name="last_name"]').type('last name')
+    cy.contains('Please enter your last name.')
+    cy.get('[name="last_name"]').should('have.class', 'error')
+  })
+
+  it('Should display an error without an email on submit.', () => {
+    cy.get('[name="first_name"]').type('first')
+    cy.get('[name="last_name"]').type('last')
     cy.get('[name="username"]').type('username')
     cy.get('[name="password"]').type('password')
     cy.get('button.success').click()
-    cy.contains('Please enter an email address.').should('have.class', 'error')
+
+    cy.contains('Please enter your email.')
+    cy.get('[name="email"]').should('have.class', 'error')
   })
 
-  it('Should display an error if the email is invalid.', () => {
-    cy.route({
-      method: 'POST',
-      url: '**/users/register',
-      response: 'fx:sign_up/invalid_email.json',
-      status: 400,
-    })
+  it('Should display an error without an email on blur.', () => {
+    cy.get('[name="email"]').click()
+    cy.get('[name="first_name"]').click()
 
-    cy.get('[name="first_name"]').type('first name')
-    cy.get('[name="last_name"]').type('last name')
+    cy.contains('Please enter your email.')
+    cy.get('[name="email"]').should('have.class', 'error')
+  })
+
+  it('Should display an error if the email is invalid on submit.', () => {
+    cy.get('[name="first_name"]').type('first')
+    cy.get('[name="last_name"]').type('last')
     cy.get('[name="email"]').type('not a valid email')
     cy.get('[name="username"]').type('username')
     cy.get('[name="password"]').type('password')
     cy.get('button.success').click()
-    cy.contains('Please enter a valid email address.').should(
-      'have.class',
-      'error',
-    )
+
+    cy.contains('Please enter a valid email.')
+    cy.get('[name="email"]').should('have.class', 'error')
+  })
+
+  it('Should display an error if the email is invalid on blur.', () => {
+    cy.get('[name="email"]').type('not a valid email')
+    cy.get('[name="username"]').click()
+
+    cy.contains('Please enter a valid email.')
+    cy.get('[name="email"]').should('have.class', 'error')
   })
 
   it('Should display an error if the email is in use.', () => {
@@ -91,29 +98,33 @@ describe('Sign Up', () => {
       status: 409,
     })
 
-    cy.get('[name="first_name"]').type('first name')
-    cy.get('[name="last_name"]').type('last name')
+    cy.get('[name="first_name"]').type('first')
+    cy.get('[name="last_name"]').type('last')
     cy.get('[name="email"]').type('already@being.used')
     cy.get('[name="username"]').type('username')
     cy.get('[name="password"]').type('password')
     cy.get('button.success').click()
+
     cy.contains('This email is already in use.').should('have.class', 'error')
   })
 
-  it('Should display an error without a username.', () => {
-    cy.route({
-      method: 'POST',
-      url: '**/users/register',
-      response: 'fx:sign_up/no_username.json',
-      status: 400,
-    })
-
-    cy.get('[name="first_name"]').type('first name')
-    cy.get('[name="last_name"]').type('last name')
+  it('Should display an error without a username on submit.', () => {
+    cy.get('[name="first_name"]').type('first')
+    cy.get('[name="last_name"]').type('last')
     cy.get('[name="email"]').type('test@test.test')
     cy.get('[name="password"]').type('password')
     cy.get('button.success').click()
-    cy.contains('Please enter a username.').should('have.class', 'error')
+
+    cy.contains('Please enter a username.')
+    cy.get('[name="username"]').should('have.class', 'error')
+  })
+
+  it('Should display an error without a username on blur.', () => {
+    cy.get('[name="username"]').click()
+    cy.get('[name="email"]').click()
+
+    cy.contains('Please enter a username.')
+    cy.get('[name="username"]').should('have.class', 'error')
   })
 
   it('Should display an error if the username is in use.', () => {
@@ -124,51 +135,45 @@ describe('Sign Up', () => {
       status: 409,
     })
 
-    cy.get('[name="first_name"]').type('first name')
-    cy.get('[name="last_name"]').type('last name')
+    cy.get('[name="first_name"]').type('first')
+    cy.get('[name="last_name"]').type('last')
     cy.get('[name="email"]').type('test@test.test')
+    cy.get('[name="username"]').type('already_in_use')
     cy.get('[name="password"]').type('password')
     cy.get('button.success').click()
+
     cy.contains('This username is already taken.').should('have.class', 'error')
   })
 
-  it('Should display an error without a password.', () => {
-    cy.route({
-      method: 'POST',
-      url: '**/users/register',
-      response: 'fx:sign_up/bad_password.json',
-      status: 400,
-    })
-
-    cy.get('[name="first_name"]').type('first name')
-    cy.get('[name="last_name"]').type('last name')
+  it('Should display an error without a password on submit.', () => {
+    cy.get('[name="first_name"]').type('first')
+    cy.get('[name="last_name"]').type('last')
     cy.get('[name="email"]').type('test@test.test')
     cy.get('[name="username"]').type('username')
     cy.get('button.success').click()
-    cy.contains('Your password must be at least 5 characters long.').should(
-      'have.class',
-      'error',
-    )
+
+    cy.contains('Please enter a password.')
+    cy.get('[name="password"]').should('have.class', 'error')
+  })
+
+  it('Should display an error without a password on blur.', () => {
+    cy.get('[name="password"]').click()
+    cy.get('[name="email"]').click()
+
+    cy.contains('Please enter a password.')
+    cy.get('[name="password"]').should('have.class', 'error')
   })
 
   it('Should display an error if password is less than 5 characters long.', () => {
-    cy.route({
-      method: 'POST',
-      url: '**/users/register',
-      response: 'fx:sign_up/bad_password.json',
-      status: 400,
-    })
-
-    cy.get('[name="first_name"]').type('first name')
-    cy.get('[name="last_name"]').type('last name')
+    cy.get('[name="first_name"]').type('first')
+    cy.get('[name="last_name"]').type('last')
     cy.get('[name="email"]').type('test@test.test')
     cy.get('[name="username"]').type('username')
     cy.get('[name="password"]').type('123')
     cy.get('button.success').click()
-    cy.contains('Your password must be at least 5 characters long.').should(
-      'have.class',
-      'error',
-    )
+
+    cy.contains('Your password must be at least 5 characters long.')
+    cy.get('[name="password"]').should('have.class', 'error')
   })
 
   it('Should successfully register, and log in the user.', () => {
