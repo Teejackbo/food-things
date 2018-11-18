4e07c4da5d3af142bc8c4638f370d86be7348f8b
Jack Coldrick <jcldrk@gmail.com>
2018-11-18 00:06:22 +0000

    Update snapshots.

--- food-things-frontend/tests/unit/views/Login.spec.js
+++ food-things-frontend/tests/unit/views/Login.spec.js
@@ -1,7 +1,11 @@
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import { shallowMount } from '@vue/test-utils'
 import Login from '@/views/Login'
 import registerGlobals from '../components/global'
 import $store from '@/store'
+
+Vue.use(Vuelidate)
 
 registerGlobals()
 
